23f82a9fe840d1355106112f0c9a43ccbee81ea9
Krishna <kaddala@salesforce.com>
2019-10-12 15:00:41 -0700

    adding logic for displaying text on the HTML page along with recording userChoice to dispplay and record prelimnary wins vs losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,19 @@ var losses = 0;
 var guessesLeft = 9;
 //create an Array with the list of letters A-Z
 var computerChoice = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//create variables to display on the HTML page
+var directionsText = document.getElementById("pressKeyToStartTheGame");
+var numberOfWins = document.getElementById("wins");
+var numberOfLosses = document.getElementById("losses");
+var guessLeft = document.getElementById("guessLeft");
+var userMadeTheChoice = document.getElementById("userGuessSoFar");
 //create a function for onKeyUp
 document.onkeyup = function(event) {
     var userGuess = event.key;
     console.log(userGuess);
     var computerGuess = computerChoice[Math.floor(Math.random()*computerChoice.length)];
     console.log(computerGuess);
+    
     if (userGuess===computerGuess){
         consol.log("You win!");
         wins++;
@@ -22,7 +29,15 @@ document.onkeyup = function(event) {
         guessesLeft--;
         }
     }
+    directionsText.textContent = "";
+
+    numberOfWins.textContent = "Wins:" + wins;
+    numberOfLosses.textContent = "Losses:" + losses;
+    guessLeft.textContent = "Number of Guesses Left:" + guessesLeft;
+    userMadeTheChoice.textContent = "List your Guesses:" + userGuess;
+    console.log(listYourGuess);
 }
+
 //function for user guess
 //function for computer guess
 //function compare the guess
