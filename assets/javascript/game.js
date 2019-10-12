fc8705f9b8a9728b05e589032b8e212daecdf2a2
Krishna <kaddala@salesforce.com>
2019-10-12 14:25:20 -0700

    adding pseudo code to the game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,20 @@
 //create variables for wins, losses, guessesLeft and set those to 0 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
+//set the variable to max number of guesses
+var guessesLeft = 9;
 //create an Array with the list of letters A-Z
-var lettersChosen = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lettersChosen = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 //create a function for onKeyUp
+//functyion for my guess
+//function for computer guess
+//function compare the guess
+    //within compare function do the comparison for win or loss
+    //if usechoice === computerchoice wins++ or  guessleft-- 
+//&& if guessleft ==0 reset the guessleft and increment losses by 1
+//actively concatenate and print on the screen
+//reset the game when user looses or wins
+
     $("#yourGuess").on("keyup", function(){
         for (i=0, i<10; i++;){
             
